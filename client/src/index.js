d3968cf0ddbf81bbf1aba50f89ddd9ae512cbbbd
Bui Quang <zzzquangzzzthuzzz@gmail.com>
2022-06-09 00:00:06 +0700

    Remove  Css

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,6 @@ import reportWebVitals from './reportWebVitals';
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
 
-import './assets/boxicons-2.0.7/css/boxicons.min.css'
 import './sass/index.scss'
 
 import RoutesProject from './routes/Routes';
